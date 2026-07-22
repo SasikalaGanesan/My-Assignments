e2ef1b9ddd37d06d752992b04b934deb1d266f30
SasikalaGanesan <36477216+SasikalaGanesan@users.noreply.github.com>
2026-07-22 20:34:19 +0530

    Add files via upload

---
+++ primitivedatatype.js
@@ -0,0 +1,8 @@
+const { type } = require("os")
+
+let firstname='sasikala'
+let companyname='CTS'
+let mobile=8925762253
+let isAutomation=true
+let hasPlaywright
+console.log(typeof firstname,typeof companyname,typeof mobile,typeof isAutomation,typeof hasPlaywright);
