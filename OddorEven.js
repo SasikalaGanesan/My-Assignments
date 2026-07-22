e1ce52ee6dcf9b051ac563f4aefdfcb429dfb2bd
SasikalaGanesan <36477216+SasikalaGanesan@users.noreply.github.com>
2026-07-22 21:32:37 +0530

    Add files via upload

---
+++ OddorEven.js
@@ -0,0 +1,13 @@
+function isOddorEven()
+{
+    let number='5';
+    if(number%2!=0)
+    {
+        console.log("given number is odd")
+
+    }
+    else{
+        console.log("given number is even")
+    }
+}
+isOddorEven()
