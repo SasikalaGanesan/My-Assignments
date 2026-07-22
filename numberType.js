07c93c11c193cc308427d7f3ddd1f790d7dd3dab
SasikalaGanesan <36477216+SasikalaGanesan@users.noreply.github.com>
2026-07-22 22:16:21 +0530

    Add files via upload

---
+++ numberType.js
@@ -0,0 +1,18 @@
+function numberTypeis()
+{
+    let number=-10;
+    if(number>0)
+    {
+        console.log("given number is Positive")
+
+    }
+    else if(number<0)
+    {
+        console.log("given number is Negative")
+    }
+    else{
+        console.log("given number is zero")
+    }
+}
+numberTypeis()
+
